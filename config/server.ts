721e82fc923e378150e7aa40c8b78fd37b675227
Idahir2406 <ipincay5178@utm.edu.ec>
2026-05-28 17:44:52 -0500

    Refactor server configuration: update PUBLIC_URL environment variable in server.ts for improved deployment flexibility.

--- config/server.ts
+++ config/server.ts
@@ -2,7 +2,7 @@ import type { Core } from '@strapi/strapi';
 
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
   host: env('HOST', '0.0.0.0'),
-  url: env('PUBLIC_URL', 'http://localhost:1337'),
+  url: env('PUBLIC_URL'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
