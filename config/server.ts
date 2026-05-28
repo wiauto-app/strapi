fc4eac2915bc9a4b0c0b4296a87b53ea27aebd8d
Idahir2406 <ipincay5178@utm.edu.ec>
2026-05-28 18:03:59 -0500

    reverse proxy false

--- config/server.ts
+++ config/server.ts
@@ -3,7 +3,6 @@ import type { Core } from '@strapi/strapi';
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
   host: env('HOST', '0.0.0.0'),
   url: env('PUBLIC_URL'),
-  proxy: env.bool('SERVER_PROXY', true),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
