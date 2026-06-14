c625bbc4a549ecb9d420200a819d6b88a1b7e37c
Idahir2406 <ipincay5178@utm.edu.ec>
2026-06-13 19:40:45 -0500

    team schema

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -807,6 +807,7 @@ export interface ApiSobreNosotroSobreNosotro extends Struct.SingleTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    equipo: Schema.Attribute.Component<'about.team', true>;
     imagen: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
