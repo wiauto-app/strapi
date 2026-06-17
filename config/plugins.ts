93acacec349ea41d1b70a07129bcfedb74351d4f
Idahir2406 <ipincay5178@utm.edu.ec>
2026-06-17 00:27:26 -0500

    Add strapi-plugin-img-convert dependency and enhance image upload configuration with new breakpoints and optimization settings for various formats.

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,6 +3,30 @@ import type { Core } from '@strapi/strapi';
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
   upload: {
     config: {
+      breakpoints: {
+        xlarge: 1566,
+        large: 1280,
+        medium: 768,
+        small: 640,
+      },
+      optimizeSettings: {
+        jpeg: {
+          quality: 85,
+        },
+        png: {
+          compressionLevel: 9,
+          effort: 6,
+        },
+        webp: {
+          quality: 80,
+          effort: 6,
+        },
+        avif: {
+          quality: 60,
+          effort: 8,
+          chromaSubsampling: '4:2:0',
+        },
+      },
       provider: "aws-s3",
       providerOptions: {
         s3Options: {
@@ -18,7 +42,7 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
           },
         },
       },
-    }
+    },
   }
 });
 
